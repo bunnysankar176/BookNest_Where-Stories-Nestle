f30b7f0b90c064d04dc075f23a3ed383eff0c8a1
bunnysankar176 <sankardonga27@gmail.com>
2026-03-01 01:55:51 +0530

    Update useDebounce.js

--- Project Files/Frontend/src/hooks/useDebounce.js
+++ Project Files/Frontend/src/hooks/useDebounce.js
@@ -1,1 +1,19 @@
+import { useRef } from "react";
 
+const useDebounce = (callback, delay = 500) => {
+  const timerRef = useRef(null);
+
+  const debouncedFunction = (...args) => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+
+    timerRef.current = setTimeout(() => {
+      callback(...args);
+    }, delay);
+  };
+
+  return debouncedFunction;
+};
+
+export default useDebounce;
